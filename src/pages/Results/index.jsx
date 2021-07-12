f80647b3f0cf2a5e051f36a125249648ce9c3d83
unknown <ferchichi.yasmine@yahoo.com>
2021-07-12 13:03:45 +0100

    first commit

---
+++ src/pages/Results/index.jsx
@@ -0,0 +1,15 @@
+import { useContext } from 'react'
+import { SurveyContext } from '../../utils/context'
+
+function Results() {
+  const { answers } = useContext(SurveyContext)
+  console.log(answers)
+
+  return (
+    <div>
+      <h1>Résultats</h1>
+    </div>
+  )
+}
+
+export default Results
